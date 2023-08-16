b72dc962a9a2e51f9e63de771d3f4fbf005e87bb
AngelaNwokoji <141584626+AngelaNwokoji@users.noreply.github.com>
2023-08-16 03:56:06 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+// array 1
+
+const colors = ['blue',
+ 'green', 
+ 'purple',
+  'black', 
+  'orange',
+   'whites',
+];           
+console.log(colors);
+
+// array 2
+
+let lang = ['c#',
+    'php', 
+    'python',
+     'java', 
+     'kotlin',
+      'c++',
+];
+console.log(lang);
+
+
+// Question  2
+
+// arithmetic functions
+function add(num1, num2, num3){
+    let sum =  num1 + num2 +  num3;
+    console.log(sum); 
+}
+add(4,6,8);
+
+
+function diff(num1, num2, ){
+    let diff =  num1 - num2;
+    console.log(diff); 
+}
+diff(8, 5);
+
+function product(num1, num2, num3){
+    let product =  num1 * num2 *  num3;
+    console.log(product); 
+}
+product(4,6,8);
+
+function divide(num1, num2){
+    let divide =  num1 / num2;
+    console.log(divide); 
+}
+divide(60,4);
